4325a43047144bdebde7a810b7ff8272ae1d3549
VereskQA <ik.veresk@gmail.com>
2022-01-12 08:37:54 +0300

    hw2 task1-3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,41 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  max = arr[0];
+  min = arr[0];
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  avg = sum / arr.length;
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+  let result = arr.reduce((a, b) => a + b); 
+    return result;
 }
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
   
-  return max;
+function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let result = worker(arrOfArr[i])
+    if (result > max) {
+      max = result;
+    }
+  }
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let arrParams = getArrayParams(arr);
+  return Math.abs(arrParams.max - arrParams.min);
 }
