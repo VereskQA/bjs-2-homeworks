0d554285faeecec85bb7a8ba1846b7f8a007c5da
Veresk <ik.veresk@gmail.com>
2022-01-14 08:59:21 +0300

    hw4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+const student1 = new Student("Ivan", "male", 18);
+const student2 = new Student("Anna", "female", 21);
+const student3 = new Student("Petr", "male", 17);
+
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){
+        this.marks = [mark]
+    } else {
+        this.marks.push(mark)
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if(this.marks === undefined){
+        this.marks = [...marks]
+    } else {
+        this.marks.push(...marks)
+    }
+}
+
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
